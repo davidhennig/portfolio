68773b429c39b567afaba3ef7fb45a27147915d1
davidhennig <davidjhennig@gmail.com>
2020-07-30 11:54:31 -0400

    testing

--- src/Components/resume.js
+++ src/Components/resume.js
@@ -8,7 +8,7 @@ const Resume = () => {
     return (
         <div className="resume-wrapper">
             <div className="resume">
-                <a target="_blank" rel="noopener noreferrer" href="google.com">
+                <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1h8gdNIr_PshOOOdWqdaM_gfoHI-N-5R_/view?usp=sharing">
                     <button>
                         View/download <br /> resume here
                     </button>
