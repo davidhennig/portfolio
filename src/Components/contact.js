d0da351f37d6f0298402c95450991c06704901cb
davidhennig <davidjhennig@gmail.com>
2020-08-06 12:07:06 -0400

    styling

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -10,19 +10,16 @@ const Contact = () => {
             <div className="contact">
                 <div className="links">
                     <div className="link">
-                        <p>Email:</p>
                         <a href="mailto:DavidJHennig@outlook.com">
                             <button>Send an email</button>
                         </a>
                     </div>
                     <div className="link">
-                        <p>LinkedIn: </p>
                         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/davidhennig/">
                             <button>Connect on LinkedIn</button>
                         </a>
                     </div>
                     <div className="link">
-                        <p>Github: </p>
                         <a target="_blank" rel="noopener noreferrer" href="https://github.com/davidhennig">
                             <button>View my Github</button>
                         </a>
--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -5,7 +5,8 @@ const Homepage = () => {
     return (
         <div className="title">
             <h1 >David J. Hennig</h1>
-            <h6 >Software Developer</h6>
+            <h3 >Software Developer</h3>
+            <h6>Located in Manchester, NH</h6>
         </div>
     )
 }
